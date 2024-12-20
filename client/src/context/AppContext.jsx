d89871973438ebd1e23159d0ab61c6759b5f5a04
Nitesh Shivhare <119953531+Nitesh4256@users.noreply.github.com>
2024-12-20 18:13:28 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 import axios from "axios";
 export const AppContextProvider = (props) => {
   // axios.defaults.withCredentials = true;
-  const backendUrl = "https://authentication-backend-8liv.onrender.com/";
+  const backendUrl = "https://authentication-backend-8liv.onrender.com";
   const [isLoggedin, setIsLoggedin] = useState(false);
   const [userData, setUserData] = useState(false);
 

dff95c644b687421002f988993b29ea5633868e4
Nitesh4256 <119953531+Nitesh4256@users.noreply.github.com>
2024-12-21 12:17:34 +0530

    final change

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -37,8 +37,8 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -96,6 +96,9 @@ export const login = async (req, res) => {
     });
 
     res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -115,8 +118,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
     });
 
     return res.json({
